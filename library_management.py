1b885266ead292b9019e47383fe909016907d372
Mary <wambuimuthima1@gmail.com>
2025-08-04 00:10:48 +0300

    modified files

---
+++ library_management.py
@@ -0,0 +1,50 @@
+# library_management.py
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # private attribute
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []  # private list of books
+
+    def add_book(self, book):
+        """Add a book to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by its title."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return
+        print(f"Book '{title}' is not available for checkout.")
+
+    def return_book(self, title):
+        """Return a book by its title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return
+        print(f"Book '{title}' was not checked out or does not exist.")
+
+    def list_available_books(self):
+        """List all books that are currently available."""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
--- simple_calculator.py
+++ simple_calculator.py
@@ -1,14 +1,14 @@
 class SimpleCalculator:
-    def add(self, a, b):
+    def addition(self, a, b):
         return a + b
     
-    def subtract(self, a, b):
+    def subtraction(self, a, b):
         return a - b
     
-    def multiply(self, a, b):
+    def multiplication(self, a, b):
         return a * b
     
-    def divide(self, a, b):
+    def division(self, a, b):
         if b == 0:
             raise ValueError("Cannot divide by zero")
         return a / b
