9c8ac9d8d45a2b838ee764f46fd9aa4aa1dadf18
Mary <wambuimuthima1@gmail.com>
2025-08-10 19:13:50 +0300

    modified oop

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,5 +1,5 @@
 #import sys
-from robust_division_calculator import safe_divide
+#from robust_division_calculator import safe_divide
 
 #def main():
  #   if len(sys.argv) != 3:
@@ -39,3 +39,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
